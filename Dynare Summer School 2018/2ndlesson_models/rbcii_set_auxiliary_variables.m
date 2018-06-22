1ea1656e1db459a5124a84bc6d38b667a7e88b50
anorring <anni.norring@helsinki.fi>
2018-06-22 13:39:32 +0300

    housekeeping

--- Dynare Summer School 2018/2ndlesson_models/rbcii_set_auxiliary_variables.m
+++
@@ -1,7 +0,0 @@
-function y = rbcii_set_auxiliary_variables(y, x, params)
-%
-% Status : Computes static model for Dynare
-%
-% Warning : this file is generated automatically by Dynare
-%           from model file (.mod)
-
--- Dynare Summer School 2018/7thlesson_models/gsa/nk1/nk2_set_auxiliary_variables.m
+++
@@ -1,8 +0,0 @@
-function y = nk2_set_auxiliary_variables(y, x, params)
-%
-% Status : Computes static model for Dynare
-%
-% Warning : this file is generated automatically by Dynare
-%           from model file (.mod)
-
-y(3)=y(1);
